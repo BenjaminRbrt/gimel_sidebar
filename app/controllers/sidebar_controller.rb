99626a4fbce631d01be8b4288796f68891bac58d
Redmine Administrator <redmine@EvoNet>
2025-05-07 16:12:41 +0200

    sidebar

--- app/controllers/sidebar_controller.rb
+++ app/controllers/sidebar_controller.rb
@@ -7,87 +7,109 @@ class SidebarController < ApplicationController
   before_action :authorize
 
   def edit
-    logger.info "edit"
+    logger.info "[SIDEBAR] => Action : edit"
+    logger.info "[SIDEBAR] Paramètres reçus : #{sidebar_params.inspect}"
+  
     if sidebar_params[:content_type].present?
-      logger.info "edit if sidebar_params[:content_type].present?"
+      logger.info "[SIDEBAR] Content type détecté : #{sidebar_params[:content_type]}"
+  
       if @sidebar
-        logger.info "edit if sidebar"
+        logger.info "[SIDEBAR] Mise à jour du contenu existant (ID: #{@sidebar.id})"
         if @sidebar.update(sidebar_params)
-          logger.info "edit if sidebar update"
+          logger.info "[SIDEBAR] Mise à jour réussie"
           flash[:notice] = l(:notice_successful_update)
+        else
+          logger.warn "[SIDEBAR] Échec de la mise à jour : #{@sidebar.errors.full_messages.join(', ')}"
         end
       else
-        logger.info "edit else !sidebar"
+        logger.info "[SIDEBAR] Nouveau contenu à créer"
         @sidebar = SidebarContent.new(sidebar_params.merge(project: @project))
+  
         if @sidebar.save
-          logger.info "edit sidebar save"
+          logger.info "[SIDEBAR] Création réussie (ID: #{@sidebar.id})"
           flash[:notice] = l(:notice_successful_create)
-        end
-        unless @sidebar.save
-          logger.error "Sidebar save failed: #{@sidebar.errors.full_messages.join(', ')}"
+        else
+          logger.error "[SIDEBAR] Échec de la création : #{@sidebar.errors.full_messages.join(', ')}"
         end
       end
     else
-      logger.info "edit else !sidebar_params[:content_type].present?"
+      logger.info "[SIDEBAR] Aucun content_type fourni. Suppression si existant..."
       if @sidebar&.destroy
-        logger.info "edit if destroy"
+        logger.info "[SIDEBAR] Suppression réussie (ID: #{@sidebar.id})"
         flash[:notice] = l(:notice_successful_delete)
         @sidebar = nil
+      else
+        logger.warn "[SIDEBAR] Rien à supprimer ou suppression échouée"
       end
     end
-
+  
     respond_to do |format|
       format.html do
+        logger.info "[SIDEBAR] Redirection HTML vers l'onglet sidebar du projet"
         redirect_to controller: 'projects', action: 'settings', id: @project, tab: 'sidebar', sidebar: sidebar_params
+        render partial: 'projects/settings/sidebar'
       end
       format.js do
+        logger.info "[SIDEBAR] Réponse JS avec mise à jour de l'onglet sidebar"
         @notice = flash.discard(:notice)
-        logger.info "controller edit"
-        render partial: 'projects/settings/sidebar'
+        #render partial: 'projects/settings/sidebar'
+        render action: 'edit'
       end
     end
   end
 
   def preview
-    logger.info "preview"
-    logger.info "Params dans preview: #{params.inspect}"
+    logger.info "[SIDEBAR] => Action : preview"
+    logger.info "[SIDEBAR] Paramètres reçus : #{params.inspect}"
+  
     return render_403 unless params[:sidebar]
-
+  
     @previewed = @sidebar
-
-    case params[:sidebar][:content_type]
+    content_type = params[:sidebar][:content_type]
+  
+    case content_type
     when 'text'
-      logger.info "text"
+      logger.info "[SIDEBAR][PREVIEW] Aperçu en mode texte"
       @text = params[:sidebar].dig(:content, :text)
       render partial: 'common/preview'
     when 'wiki'
-      logger.info "wiki"
-      page = @project&.wiki && params[:sidebar][:content] ? @project.wiki.find_page(params[:sidebar][:content][:wiki]) : nil
+      logger.info "[SIDEBAR][PREVIEW] Aperçu d'une page wiki"
+      wiki_page_name = params[:sidebar].dig(:content, :wiki)
+      page = @project&.wiki&.find_page(wiki_page_name)
       @text = page&.text.to_s
       render partial: 'common/preview'
     when 'html'
-      logger.info "html"
+      logger.info "[SIDEBAR][PREVIEW] Aperçu HTML brut"
       html = "<fieldset class=\"preview\"><legend>#{l(:label_preview)}</legend>"
       html << (params[:sidebar].dig(:content, :html) || '')
       html << "</fieldset>"
       render html: html.html_safe
     else
-      logger.info "Aucun"
+      logger.warn "[SIDEBAR][PREVIEW] Type de contenu inconnu ou non pris en charge : #{content_type}"
       render_403
     end
   end
-
+  
   def pages
+    logger.info "[SIDEBAR] => Action : pages (visibilité)"
+  
     @visibility ||= SidebarSetting.new(project: @project)
+  
     if request.post?
+      logger.info "[SIDEBAR] Enregistrement des pages visibles : #{params[:pages]}"
       @visibility.pages = params[:pages]
+  
       if @visibility.save
+        logger.info "[SIDEBAR] Visibilité enregistrée avec succès"
         flash[:notice] = l(:notice_successful_update)
+      else
+        logger.warn "[SIDEBAR] Échec de l'enregistrement de la visibilité"
       end
+  
       redirect_back_or_to controller: 'projects', action: 'settings', id: @project, tab: 'sidebar'
     end
   end
-
+   
   private
 
   def find_project
@@ -98,6 +120,7 @@ class SidebarController < ApplicationController
 
   def find_sidebar_content
     @sidebar = SidebarContent.find_by(project_id: @project.id)
+    Rails.logger.info "Sidebar found: #{@sidebar.present?}"
   end
 
   def find_sidebar_visibility
--- lib/sidebar_page.rb
+++ lib/sidebar_page.rb
@@ -1,71 +1,75 @@
 class SidebarPage
-    include Redmine::I18n
-  
-    attr_reader :name, :urls, :caption
-  
-    def initialize(name, urls, options = {})
-      @name = name
-      @urls = urls
-      @caption = l(options[:caption] || "label_#{name}")
-    end
-  
-    def self.available_pages
-      if @available_pages
-        @available_pages
-      else
-        @available_pages = [
-          self.new(:news_list,        { :news => :index }),
-          self.new(:news,             { :news => :show }),
-          self.new(:version,          { :versions => :show }),
-          self.new(:files_list,       { :files => :index, :projects => :list_files }),
-          self.new(:new_issue,        { :issues => :new }, :caption => :label_issue_new),
-          self.new(:forums_list,      { :boards => :index }),
-          self.new(:forum,            { :boards => :show, :messages => :show }, :caption => :label_board),
-          self.new(:repository,       { :repositories => :show }),
-          self.new(:revisions,        { :repositories => [ :changes, :revisions ] }, :caption => :label_revision_plural),
-          self.new(:repository_entry, { :repositories => :entry }),
-          self.new(:annotation,       { :repositories => :annotate }),
-          self.new(:differences_view, { :repositories => :diff }),
-          self.new(:settings,         { :projects => :settings }),
-          self.new(:wiki,             { :wiki => :show }, :caption => :label_wiki)  # Ajout de la page wiki/show
-        ]
-      end
-    end
-  
-    def self.sidebar_enabled?(path, project)
-      unless @path_to_page_cache
-        @path_to_page_cache = {}
-        self.available_pages.each do |page|
-          page.urls.each do |controller, actions|
-            if actions.is_a?(Array)
-              actions.each do |action|
-                @path_to_page_cache["#{controller}/#{action}"] = page
-              end
-            else
-              @path_to_page_cache["#{controller}/#{actions}"] = page
-            end
+  include Redmine::I18n
+
+  attr_reader :name, :urls, :caption
+
+  def initialize(name, urls, options = {})
+    @name = name
+    @urls = urls
+    @caption = l(options[:caption] || "label_#{name}")
+  end
+
+  def self.available_pages
+    return @available_pages if @available_pages
+
+    @available_pages = [
+      new(:news_list,        { news: :index }),
+      new(:news,             { news: :show }),
+      new(:version,          { versions: :show }),
+      new(:files_list,       { files: :index, projects: :list_files }),
+      new(:new_issue,        { issues: :new }, caption: :label_issue_new),
+      new(:forums_list,      { boards: :index }),
+      new(:forum,            { boards: :show, messages: :show }, caption: :label_board),
+      new(:repository,       { repositories: :show }),
+      new(:revisions,        { repositories: [:changes, :revisions] }, caption: :label_revision_plural),
+      new(:repository_entry, { repositories: :entry }),
+      new(:annotation,       { repositories: :annotate }),
+      new(:differences_view, { repositories: :diff }),
+      new(:settings,         { projects: :settings }),
+
+       # AJOUTS MANQUANTS :
+      new(:project_overview, { projects: :show }, caption: :label_overview),
+      new(:issue_list,       { issues: :index }, caption: :label_issue_plural),
+      new(:calendar,         { calendars: :show }, caption: :label_calendar),
+      new(:activity,         { activities: :index }, caption: :label_activity),
+      new(:wiki,             { wiki: [:show, :index] }, caption: :label_wiki),
+      new(:time_entries, { timelog: :index }, caption: :label_spent_time),
+      new(:contacts, { contacts: :index }, caption: :label_contact_plural)
+
+
+    ]
+  end
+
+  def self.sidebar_enabled?(path, project)
+    unless @path_to_page_cache
+      @path_to_page_cache = {}
+      available_pages.each do |page|
+        page.urls.each do |controller, actions|
+          Array(actions).each do |action|
+            key = "#{controller}/#{action}"
+            @path_to_page_cache[key] = page
           end
         end
       end
-  
-      sidebar_page = @path_to_page_cache[path]
-      if sidebar_page
-        global_settings = Setting.plugin_sidebar
-        project_settings = project ? SidebarSetting.find_by_project_id(project.id) : nil
-  
-        case global_settings['policy']
-        when 'global'
-          global_settings['pages'] && global_settings['pages'].include?(sidebar_page.name.to_s)
-        when 'disable'
-          project_settings ? project_settings.enabled?(sidebar_page.name) : (global_settings['pages'] && global_settings['pages'].include?(sidebar_page.name.to_s))
-        when 'configurable', 'project'
-          project_settings && project_settings.enabled?(sidebar_page.name)
-        else
-          false
-        end
-      else
-        false
-      end
+    end
+
+    sidebar_page = @path_to_page_cache[path]
+    Rails.logger.info "[SIDEBAR] PATH = #{path} → PAGE = #{sidebar_page&.name || 'NONE'}"
+
+    return false unless sidebar_page
+
+    global_settings = Setting.plugin_sidebar
+    project_settings = project ? SidebarSetting.find_by_project_id(project.id) : nil
+
+    case global_settings['policy']
+    when 'global'
+      global_settings['pages']&.include?(sidebar_page.name.to_s)
+    when 'disable'
+      project_settings ? project_settings.enabled?(sidebar_page.name) : global_settings['pages']&.include?(sidebar_page.name.to_s)
+    when 'configurable', 'project'
+      project_settings&.enabled?(sidebar_page.name)
+    else
+      false
     end
   end
-  
+end
